ea00e77058ee196534c635ffed8294f8aff06b71
Bruce.wu <bruce.wu@optnet.site>
2024-09-12 09:45:24 +0800

    支持运行liquibase原始命令

--- src/main/java/io/jenkins/plugins/configops/database/LiquibaseRawCmdStep.java
+++ src/main/java/io/jenkins/plugins/configops/database/LiquibaseRawCmdStep.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.configops.database;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Util;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.remoting.VirtualChannel;
@@ -15,6 +16,7 @@ import io.jenkins.plugins.configops.utils.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -32,34 +34,56 @@ import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
 import org.jenkinsci.plugins.workflow.steps.StepExecution;
 import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
+/**
+ * 运行Liquibase原始命令
+ * <a href="https://docs.liquibase.com/commands/home.html">Liquibase Commands</a>
+ *
+ * @author Bruce.Wu
+ * @date 2024-09-12
+ */
 @Getter
 @Setter
 @ToString
 @Log
-public class LiquibaseUpdateStep extends Step implements Serializable {
+public class LiquibaseRawCmdStep extends Step implements Serializable {
 
     private static final long serialVersionUID = -3437547656737490722L;
     /**
      * Config Ops工具地址
      */
     private String toolUrl;
-
     /**
      * Database ID
      */
     private final String databaseId;
-
     /**
-     * Change log 文件
+     * 在哪个路径下执行
      */
-    private String file;
+    private String cwd;
+    /**
+     * Liquibase命令
+     */
+    private final String command;
+    /**
+     * Liquibase命令参数
+     */
+    private final String args;
 
     @DataBoundConstructor
-    public LiquibaseUpdateStep(@NonNull String databaseId, String toolUrl, @NonNull String file) {
+    public LiquibaseRawCmdStep(
+            String toolUrl, @NonNull String databaseId, String cwd, @NonNull String command, String args) {
         this.toolUrl = StringUtils.defaultIfBlank(toolUrl, Constants.DEFAULT_TOOL_URL);
         this.databaseId = databaseId;
-        this.file = file;
+        this.cwd = cwd;
+        this.command = command;
+        this.args = args;
+    }
+
+    @DataBoundSetter
+    public void setCwd(String cwd) {
+        this.cwd = cwd;
     }
 
     @Override
@@ -70,9 +94,9 @@ public class LiquibaseUpdateStep extends Step implements Serializable {
     public static class StepExecutionImpl extends SynchronousNonBlockingStepExecution<Map<String, Object>> {
         private static final long serialVersionUID = -1872031188937161306L;
 
-        private final LiquibaseUpdateStep step;
+        private final LiquibaseRawCmdStep step;
 
-        public StepExecutionImpl(@NonNull StepContext context, LiquibaseUpdateStep step) {
+        public StepExecutionImpl(@NonNull StepContext context, LiquibaseRawCmdStep step) {
             super(context);
             this.step = step;
         }
@@ -84,25 +108,29 @@ public class LiquibaseUpdateStep extends Step implements Serializable {
             if (Objects.isNull(workspace)) {
                 throw new ConfigOpsException("Step workspace is null");
             }
-
-            FilePath changelogRootPath = workspace.child(step.getFile());
-            if (!changelogRootPath.exists() || changelogRootPath.isDirectory()) {
-                throw new ConfigOpsException("Changelog root file not exists or dictionary. file:" + step.getFile());
+            FilePath cwdPath = workspace;
+            if (StringUtils.isNotBlank(step.getCwd())) {
+                cwdPath = workspace.child(step.getCwd());
             }
 
-            Logger logger = new Logger("LiquibaseUpdate", taskListener);
-            DatabaseRunLiquibaseResp resp =
-                    changelogRootPath.act(new RemoteExecutionCallable(step.getToolUrl(), step.getDatabaseId()));
+            Logger logger = new Logger("LiquibaseCommand", taskListener);
 
-            logger.log("Liquibase update return code: %s", resp.getRetcode());
+            logger.log(
+                    "cwd:%s, command:%s, args:%s",
+                    Util.fixNull(step.getCwd()), step.getCommand(), Util.fixNull(step.getArgs()));
+
+            DatabaseRunLiquibaseResp resp = cwdPath.act(new RemoteExecutionCallable(
+                    step.getToolUrl(), step.getDatabaseId(), step.getCommand(), step.getArgs()));
+
+            logger.log("Liquibase run command return code: %s", resp.getRetcode());
             if (StringUtils.isNotBlank(resp.getStdout())) {
-                logger.log("Liquibase update stdout:\n%s", resp.getStdout());
+                logger.log("Liquibase run command stdout:\n%s", resp.getStdout());
             }
             if (StringUtils.isNotBlank(resp.getStderr())) {
-                logger.log("Liquibase update stderr:\n%s", resp.getStderr());
+                logger.log("Liquibase run command stderr:\n%s", resp.getStderr());
             }
             if (!resp.isSuccess()) {
-                throw new ConfigOpsException("Execute liquibase update unsuccessful");
+                throw new ConfigOpsException("Execute liquibase run command unsuccessful");
             }
             return Collections.emptyMap();
         }
@@ -112,21 +140,28 @@ public class LiquibaseUpdateStep extends Step implements Serializable {
         private static final long serialVersionUID = 354632146040812160L;
         private final String toolUrl;
         private final String databaseId;
+        private final String command;
+        private final String args;
 
-        public RemoteExecutionCallable(String toolUrl, String databaseId) {
+        public RemoteExecutionCallable(String toolUrl, String databaseId, String command, String args) {
             this.toolUrl = toolUrl;
             this.databaseId = databaseId;
+            this.command = command;
+            this.args = args;
         }
 
         @Override
         public DatabaseRunLiquibaseResp invoke(File f, VirtualChannel channel)
                 throws IOException, InterruptedException {
             ConfigOpsClient client = new ConfigOpsClient(toolUrl);
+            if (!Files.isDirectory(f.toPath())) {
+                throw new ConfigOpsException("Run command cwd is not a dictionary: " + f.getName());
+            }
             DatabaseRunLiquibaseReq req = DatabaseRunLiquibaseReq.builder()
                     .dbId(databaseId)
-                    .cwd(f.getParentFile().getAbsolutePath())
-                    .command("update")
-                    .args("--changelog-file " + f.getName())
+                    .cwd(f.getAbsolutePath())
+                    .command(command)
+                    .args(args)
                     .build();
             return client.databaseRunLiquibase(req);
         }
@@ -146,12 +181,12 @@ public class LiquibaseUpdateStep extends Step implements Serializable {
         @NonNull
         @Override
         public String getDisplayName() {
-            return "Liquibase Update Step";
+            return "Liquibase Raw Command Step";
         }
 
         @Override
         public String getFunctionName() {
-            return "liquibaseUpdate";
+            return "liquibaseRawCmd";
         }
     }
 }
